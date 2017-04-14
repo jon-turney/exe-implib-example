d0a086b85c81d97fe1b89108d5934e7183a45484
Jon Turney <jon.turney@dronecode.org.uk>
2017-04-14 17:35:56 +0100

    Mark func_from_executable as exported.
    Needed??? If there are no explicit exports, ld turns on --export-all-symbols

--- prog.c
+++ prog.c
@@ -5,6 +5,17 @@
 #include <dlfcn.h>
 #endif
 
+#if defined _WIN32 || defined __CYGWIN__
+  #define DLL_PUBLIC __declspec(dllexport)
+#else
+  #if defined __GNUC__
+    #define DLL_PUBLIC __attribute__ ((visibility("default")))
+  #else
+    #pragma message ("Compiler does not support symbol visibility.")
+    #define DLL_PUBLIC
+  #endif
+#endif
+
 int
 main (int argc, char **argv)
 {
@@ -25,7 +36,7 @@ main (int argc, char **argv)
 #endif
 }
 
-int
+int DLL_PUBLIC
 func_from_executable(void)
 {
   return 42;
